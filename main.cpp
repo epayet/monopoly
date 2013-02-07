73bb9cef69bb279bc204ff4fa2ea53c94ab7f826
epayet <epayet@epsi.fr>
2013-02-07 09:54:45 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: manu
+ *
+ * Created on 7 février 2013, 09:51
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+	return 0;
+}
+
